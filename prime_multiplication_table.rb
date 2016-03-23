791a9fdfac46d4a91e434c7bc9b709002d0c1ebb
Maryaeria <maryamsyed2096@gmail.com>
2016-03-23 15:12:46 -0700

    Edit specs to expect methods inside a new Prime class,  methods now inside - all specs passing

--- prime_multiplication_table.rb
+++ prime_multiplication_table.rb
@@ -1,22 +1,86 @@
 require 'benchmark'
+
+class Primes
+  #
 # now order sqrt(n) !
-def is_prime?(n)
-  if n % 2 == 0 && n != 2
-    return false
-  end
-  (3..(Math.sqrt(n).to_i)).step(2).each do |f|
-    if (n % f) == 0
+  def self.is_prime?(n)
+    if n % 2 == 0 && n != 2
       return false
     end
+    (3..(Math.sqrt(n).to_i)).step(2).each do |f|
+      if (n % f) == 0
+        return false
+      end
+    end
+    true
   end
-  true
-end
 
-# using Eratosthene's sieve, we now have O(n(log n)(log(log n)))
-def primes_array(n)
-  # until we have n numbers in the array,
-  # we will look for primes 
-  if n < 6
+  # using Eratosthene's sieve, we now have O(n(log n)(log(log n)))
+  def self.primes_array(n)
+    # until we have n numbers in the array,
+    # we will look for primes 
+    if n < 6
+      prime_storage = []
+      test_prime = 2
+      while(prime_storage.length < n) do
+        if is_prime?(test_prime)
+          prime_storage.push(test_prime)
+        end
+        test_prime += 1
+      end
+      prime_storage
+    else
+      upper_bound = n * Math.log(n) + n * Math.log(Math.log(n))
+      unfiltered = [nil, nil, *2..upper_bound]
+      (2..upper_bound.to_i).each do |i|
+        (i**2..upper_bound.to_i).step(i).each do |m|
+          if unfiltered[i]
+            unfiltered[m] = nil
+          end
+        end
+      end
+      unfiltered.compact
+    end
+  end
+
+  # ORDER  n^2 -- can this be optimized?
+  def self.prime_times_table(n)
+    primes = primes_array(n) # order n(log(n))(log(log(n)))
+    biggest_length = biggest_prime_multiple(primes).to_s.length
+
+    print " "*(biggest_length + 1)
+    primes.each do |p|
+      print " " +  " "*padding(biggest_length, p) + p.to_s
+    end
+
+    primes.each do |p|
+    puts ""
+      multiplied = [p]
+      primes.each do |prime| # order n^2
+        multiplied.push(p * prime)
+      end
+      multiplied.each do |m|
+        print " " + " "*padding(biggest_length, m) + m.to_s
+      end
+    end
+    puts ""
+  end
+
+  def self.biggest_prime_multiple(primes)
+    biggest_multiples = []
+    primes.each do |p|
+      biggest_multiples.push(primes.max * p)
+    end
+    return biggest_multiples.max
+  end
+
+  def self.padding(biggest_length, n)
+    return (biggest_length - n.to_s.length).abs
+  end
+
+  def old_primes_array(n)
+    # until we have n numbers in the array,
+    # we will look for primes 
     prime_storage = []
     test_prime = 2
     while(prime_storage.length < n) do
@@ -25,74 +89,17 @@ def primes_array(n)
       end
       test_prime += 1
     end
-    prime_storage
-  else
-    upper_bound = n * Math.log(n) + n * Math.log(Math.log(n))
-    unfiltered = [nil, nil, *2..upper_bound]
-    (2..upper_bound.to_i).each do |i|
-      (i**2..upper_bound.to_i).step(i).each do |m|
-        if unfiltered[i]
-          unfiltered[m] = nil
-        end
-      end
-    end
-    unfiltered.compact
-  end
-end
-
-# ORDER  n^2 -- can this be optimized?
-def prime_times_table(n)
-  primes = primes_array(n) # order n(log(n))(log(log(n)))
-  biggest_length = biggest_prime_multiple(primes).to_s.length
-
-  print " "*(biggest_length + 1)
-  primes.each do |p|
-    print " " +  " "*padding(biggest_length, p) + p.to_s
   end
 
-  primes.each do |p|
-  puts ""
-    multiplied = [p]
-    primes.each do |prime| # order n^2
-      multiplied.push(p * prime)
-    end
-    multiplied.each do |m|
-      print " " + " "*padding(biggest_length, m) + m.to_s
-    end
-  end
-  puts ""
-end
-
-def biggest_prime_multiple(primes)
-  biggest_multiples = []
-  primes.each do |p|
-    biggest_multiples.push(primes.max * p)
-  end
-  return biggest_multiples.max
-end
-
-def padding(biggest_length, n)
-  return (biggest_length - n.to_s.length).abs
-end
-
-def old_primes_array(n)
-  # until we have n numbers in the array,
-  # we will look for primes 
-  prime_storage = []
-  test_prime = 2
-  while(prime_storage.length < n) do
-    if is_prime?(test_prime)
-      prime_storage.push(test_prime)
-    end
-    test_prime += 1
-  end
-end
+  private_class_method :biggest_prime_multiple
+  private_class_method :padding
 
 =begin
-puts Benchmark.measure{old_primes_array(100)}
-puts Benchmark.measure{old_primes_array(200)}
-puts Benchmark.measure{primes_array(100)}
-puts Benchmark.measure{primes_array(200)}
+  puts Benchmark.measure{old_primes_array(100)}
+  puts Benchmark.measure{old_primes_array(200)}
+  puts Benchmark.measure{primes_array(100)}
+  puts Benchmark.measure{primes_array(200)}
 =end
+end
 
-prime_times_table(10)
+Primes.prime_times_table(10)
--- specs/table_spec.rb
+++ specs/table_spec.rb
@@ -4,52 +4,52 @@ require 'benchmark'
 RSpec.describe "Primes" do
   context "is_prime?" do 
     it "returns true for a prime number" do
-      expect(is_prime?(5)).to eq(true)
+      expect(Primes.is_prime?(5)).to eq(true)
     end
 
     it "returns false for a composite number" do
-      expect(is_prime?(6)).to eq(false)
+      expect(Primes.is_prime?(6)).to eq(false)
     end
 
     it "is order sqrt(n)" do
       n = 10000000000
-      basetime = Benchmark.realtime{is_prime?(n)}
-      expect(Benchmark.realtime{is_prime?(n**2)}).to be < Math.sqrt(basetime)
+      basetime = Benchmark.realtime{Primes.is_prime?(n)}
+      expect(Benchmark.realtime{Primes.is_prime?(n**2)}).to be < Math.sqrt(basetime)
     end
   end
 
   context "primes_array" do
     it "returns an array of all primes from 2 to n for n >= 6" do
-      expect(primes_array(6)).to eq([2,3,5,7,11,13])
+      expect(Primes.primes_array(6)).to eq([2,3,5,7,11,13])
     end
 
     it "returns an array of all primes from 2 to n for n < 6" do
-      expect(primes_array(4)).to eq([2,3,5,7])
+      expect(Primes.primes_array(4)).to eq([2,3,5,7])
     end
 
     it "is order n(log(n))(log(log(n)))" do
       n = 1000
-      basetime = Benchmark.realtime{primes_array(n)}
+      basetime = Benchmark.realtime{Primes.primes_array(n)}
       puts Math.log(basetime)
       upper_bound = (basetime * (Math.log(basetime))).abs
       # using (log(log(n))) results in domain errors 
-      expect(Benchmark.realtime{primes_array(n*2)}).to be < upper_bound
+      expect(Benchmark.realtime{Primes.primes_array(n*2)}).to be < upper_bound
 
     end
   end
 
   context "prime_times_table" do
     it "outputs the correct table for n = 1" do
-      expect{prime_times_table(1)}.to output("   2\n 2 4\n").to_stdout
+      expect{Primes.prime_times_table(1)}.to output("   2\n 2 4\n").to_stdout
     end
 
     it "outputs the correct table for n > 1" do
-      expect{prime_times_table(2)}.to output("   2 3\n 2 4 6\n 3 6 9\n").to_stdout
+      expect{Primes.prime_times_table(2)}.to output("   2 3\n 2 4 6\n 3 6 9\n").to_stdout
     end
 
     it "is of order n^2 time complexity" do
-      basetime = Benchmark.realtime{prime_times_table(10)}
-      expect(Benchmark.realtime{prime_times_table(20)}).to be < Math.sqrt(basetime)
+      basetime = Benchmark.realtime{Primes.prime_times_table(10)}
+      expect(Benchmark.realtime{Primes.prime_times_table(20)}).to be < Math.sqrt(basetime)
     end
   end
 end
